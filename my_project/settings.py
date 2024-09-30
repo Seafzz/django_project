ee069ab9bfe5a2394d9ab59ede6d9cbadb9c9fc0
Seafzz <163156309+Seafzz@users.noreply.github.com>
2024-09-30 15:14:52 +0000

    getting ready for Herouku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-m%v$ddd_6jr1g7n&eib4@(qvwdfb$)p6c95ioy71b*u=78p#77"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [,'.herokuapp.com']
 
 
 # Application definition
